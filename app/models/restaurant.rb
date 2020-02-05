f7e890780de83ffe7cd29825743e384b680a0627
wags83 <gpwagner@gmail.com>
2020-02-05 10:47:31 -0500

    I'm learndddinng

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,42 @@
 class Restaurant
+ 
   attr_reader :name
-
+  @@all = []
+  
   def initialize(name)
     @name = name
+
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+    Review.all.select {|review| review.restaurant == self}
+  end
+
+  def customers
+    self.reviews.map {|review| review.customer}.uniq
+  end
+
+  def total_stars
+    self.reviews.map {|review| review.rating}.sum
+  end
+
+  def num_reviews
+    self.reviews.length
+  end
+
+  def avg_star_rating
+    self.total_stars.to_f / self.num_reviews.to_f
+  end
+
+  #Either way works, not sure what the better form is.  First way splits out method responsibility more, second uses reduce function which is a higher level function and gets the code down to 1 line.
+  
+  # def avg_star_rating
+  #   self.reviews.reduce(0) {|sum, review| sum + review.rating.to_f} / self.num_reviews
+  # end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,26 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+joe = Customer.new("Joe", "Smithee")
+joe2 = Customer.new("Joe", "Block")
+rick = Customer.new("Rick", "Sanchez")
+morty = Customer.new("Morty", "Smith")
+
+sal = Restaurant.new("Fat Sal's Pizza")
+mcd = Restaurant.new("McDonald's")
+cirque = Restaurant.new("Le Cirque")
+
+review1 = Review.new(joe, sal, 6)
+review2 = Review.new(joe, mcd, 2)
+review3 = Review.new(joe, cirque, 10)
+review4 = Review.new(rick, sal, 4)
+review5 = Review.new(rick, mcd, 3)
+review6 = Review.new(morty, sal, 5)
+review7 = Review.new(joe, sal, 5)
+
+
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
