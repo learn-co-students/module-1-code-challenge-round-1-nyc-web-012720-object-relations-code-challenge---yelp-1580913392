a8714828a1990f0a7c9369f825962f8adbfaeae7
Joseph Kniskern <kniskernjoseph@yaho.com>
2020-02-05 10:46:18 -0500

    nailed it

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,28 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+
+    @@all << self 
+  end
+
+  def self.all 
+    @@all 
+  end
+
+  def reviews 
+    Review.all.select { |review| review.restaurant == self}
+  end
+
+  def customers
+    reviews.map{|review| review.customer.given_name}
+  end
+
+  def average_star_rating
+    reviews.map { |review| review.rating}.sum / reviews.length
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,28 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+
+
+
+restaurant1 = Restaurant.new("Pizza Zone")
+restaurant2 = Restaurant.new("Burger Zone")
+restaurant3 = Restaurant.new("Sloppy Joe Zone")
+
+customer1 = Customer.new("David", "Schwimmer")
+customer2 = Customer.new("Courtney", "Cox")
+customer3 = Customer.new("Jennifer", "Anniston")
+
+review1 = Review.new(customer2, restaurant3, 1)
+review2 = Review.new(customer1, restaurant1, 5)
+#review3 - Review.new("It's aight", customer3, restaurant2)
+review4 = Review.new(customer2, restaurant1, 3)
+review5 = Review.new(customer3, restaurant3, 4)
+review6 = Review.new(customer1, restaurant3, 5)
+review7 = Review.new(customer1, restaurant2, 2)
+
+
+
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
