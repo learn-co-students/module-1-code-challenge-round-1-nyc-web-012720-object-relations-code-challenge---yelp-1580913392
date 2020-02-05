3446b8f56b5ba274e6846235e7339fec26a57410
github username <github email address>
2020-02-05 10:51:40 -0500

    nailed it

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,55 @@
 class Customer
   attr_accessor :given_name, :family_name
 
+  @@all = []
+
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+
+    @@all << self
   end
 
   def full_name
     "#{given_name} #{family_name}"
   end
+
+  def self.all 
+    @@all 
+  end 
+
+  def restaurants
+    reviews = Review.all.select do |review|
+      review.customer == self
+    end
+
+    restaurants = reviews.map do |review|
+      review.customer
+    end 
+
+    restaurants.uniq
+  end 
+
+  def reviews #helper
+    Review.all.select do |review|
+      review.customer == self
+    end 
+  end 
+
+  def num_reviews
+    reviews.length 
+  end 
+
+  def self.find_by_name(name_string)
+    self.all.find do |cust|
+      cust.full_name == name_string
+    end 
+  end
+
+  def self.find_all_by_given_name(first_name)
+    self.all.select do |customer|
+      customer.given_name == first_name
+    end
+  end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,47 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+
+    @@all << self
   end
+
+  def self.all 
+    @@all
+
+  end 
+
+  def customers
+    
+    reviews = Review.all.select do |review|
+      review.restaurant == self
+    end
+
+    customers = reviews.map do |review|
+      review.customer
+    end 
+
+    customers.uniq
+  
+  end 
+
+  def reviews #helper
+    Review.all.select do |review|
+      review.restaurant == self
+    end 
+  end
+
+  def average_star_rating
+    added_reviews = 0
+    reviews.each do |review|
+      added_reviews += review.rating 
+    end
+
+    added_reviews / reviews.length.to_f
+
+  end 
 
 end
