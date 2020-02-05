9d2f71f1d6cefff693187c4a547f603f618fe9b9
haltonba <haltonba94@gmail.com>
2020-02-05 10:43:54 -0500

    Nailed It

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,28 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+    Review.all.select {|review| review.restaurant === self}
+  end
+
+  def customers
+    reviews.map {|review| review.customer}.uniq
+  end
+
+  def average_star_rating
+    rating_sum = reviews.map {|review| review.rating}.sum.to_f
+    rating_sum / reviews.length
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,30 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+cust1 = Customer.new("Halton", "Bagley")
+cust2 = Customer.new("Jeff", "Jones")
+cust3 = Customer.new("Chris", "Maxwell")
+cust4 = Customer.new("Caroline", "Black")
+cust5 = Customer.new("Chris", "Johnson")
+
+
+rest1 = Restaurant.new("McDonalds")
+rest2 = Restaurant.new("BK")
+rest3 = Restaurant.new("Wendys")
+rest4 = Restaurant.new("KFC")
+
+review1 = Review.new(9, cust1, rest1)
+review2 = Review.new(102, cust1, rest2)
+review3 = Review.new(3, cust2, rest3)
+review4 = Review.new(7, cust2, rest4)
+review5 = Review.new(77.4, cust3, rest1)
+review6 = Review.new(34, cust3, rest2)
+review7 = Review.new(1, cust4, rest3)
+review8 = Review.new(8, cust4, rest4)
+
+yo = rest1.average_star_rating
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
